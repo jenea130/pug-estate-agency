291055c1b92f8e8ec5840f44c0230f37b648ca47
jenea130 <jenea130@mail.ru>
2023-10-05 00:00:26 +0300

    fix: home-intro

--- src/assets/jquery/custom-jquery.js
+++ src/assets/jquery/custom-jquery.js
@@ -1,27 +1,26 @@
 jQuery(document).ready(function ($) {
-    console.log("hello");
 
-    // const slider = $(".home-intro__wrap")
+  const slider = $(".home-intro__wrap")
 
-    // if (slider) {
-    //     introSlider();
-    // }
+  if (slider) {
+      introSlider();
+  }
 
-    // function introSlider() {
-    //     slider.slick({
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //         speed: 1000,
-    //         arrows: false,
-    //         dots: false
-    //     })
-    //     const intro__arrow_prev = $('.home-intro__arrow--prev')
-    //     const intro__arrow_next = $('.home-intro__arrow--next')
-    //     intro__arrow_prev.on('click', function () {
-    //         slider.slick('slickPrev')
-    //     })
-    //     intro__arrow_next.on('click', function () {
-    //         slider.slick('slickNext')
-    //     })
-    // }
+  function introSlider() {
+      slider.slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 1000,
+          arrows: false,
+          dots: false
+      })
+      const intro__arrow_prev = $('.home-intro__arrow--prev')
+      const intro__arrow_next = $('.home-intro__arrow--next')
+      intro__arrow_prev.on('click', function () {
+          slider.slick('slickPrev')
+      })
+      intro__arrow_next.on('click', function () {
+          slider.slick('slickNext')
+      })
+  }
 });
