c5e63060557a0b1a07c5b3a3c4cebd445f0727ed
jenea130 <jenea130@mail.ru>
2023-10-04 22:31:39 +0300

    fix: menu

--- src/assets/js/src/modules/libs.js
+++
@@ -1,4 +0,0 @@
-let divide = function(first, second = 2) {
-	return first / second;
-};
-export default divide;
--- src/assets/js/src/modules/some.js
+++
@@ -1,6 +0,0 @@
-import divide from "./libs";
-
-export default () => {
-	console.log(divide(10, 2));
-	console.log(divide(4));
-};
